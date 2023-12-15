b18559816a196020472a4f1c2bc0a1b12584df01
user <user@172.31.98.239>
2023-12-15 14:45:57 +0900

    12/15 ４限目

--- src/p01/Hero.java
+++ src/p01/Hero.java
@@ -1,14 +1,30 @@
 package p01;
+
 public class Hero {
 	int id;
 	String name;
-	
+
 	public Hero(int id, String name) {
 		this.id = id;
 		this.name = name;
 	}
-	
+
+	// 適切な文字列表現を返すtoStringメソッドとしてオーバーライド
 	public String toString() {
 		return "ID:" + this.id + " NAME:" + this.name;
+	}
+
+	// equalsメソッドをオーバーライド
+	public boolean equals(Object obj) {
+		// ①まずオブジェクトが同一なら無条件にtrueを返す
+		if (this == obj) {
+			return true;
+		}
+		// ②次に、objをHero型にキャストできない場合は無条件にfalseを返す
+		if (!(obj instanceof Hero)) { // [!] → 論理否定演算子（論理反転）
+			return false;
+		}
+//		Hero h = (Hero)obj; // Heroインスタンスにキャストする
+		return this.id == ((Hero) obj).id;
 	}
 }
--- src/p01/Main.java
+++ src/p01/Main.java
@@ -4,11 +4,22 @@ public class Main {
 	public static void main(String[] args) {
 		Hero h = new Hero(1, "みなと");
 		System.out.println(h.toString());
-		
+
 		// toString()は、インスタンス（変数）名だけで自動的に呼び出される
 		System.out.println(h);
-		
+
 		String word = h + "サンプルテキスト";
 		System.out.println(word);
+
+		String name = "みなと";
+
+		Hero h2 = new Hero(1, "みなと");
+//		Hero h2 = h; //同じ番地を見ているため等しいと表示される
+
+		if (h.equals(h2)) {
+			System.out.println("等しい");
+		} else {
+			System.out.println("等しくない");
+		}
 	}
 }
